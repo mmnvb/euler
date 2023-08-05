fcf167504c80f2292f4b8636cb360d965acfdb60
saidasror69876 <128883720+saidasror69876@users.noreply.github.com>
2023-08-05 23:07:43 +0500

    solve 004

--- 1/004/es.py
+++ 1/004/es.py
@@ -1,9 +1,8 @@
 e = []
-for i in range(600,1000):
-    for g in range(600,1000):
+for i in range(100,1000):
+    for g in range(100,1000):
         result = i * g
         re = int(str(result)[::-1])
         if result == re:
-            s = i, "*" ,g ,"=" ,result
-            e.append(s)
+            e.append(result)
 print(max(e))
