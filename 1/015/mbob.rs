9a678f0e673eb4f4f883d75f00edf409399700c9
muminovbob <bobomurodm2004@gmail.com>
2023-08-24 09:32:21 +0500

    Solved 15 using Rust (fixed)

--- 1/015/mbob.rs
+++ 1/015/mbob.rs
@@ -8,15 +8,14 @@ fn fac(n: u128, start:u128) -> u128{
 }
 
 fn main(){
-    // this is the size of the grid 
+    // this is the size of the grid (since we have equal sizes let's use 1 variable only)
     let a = 20;
-    let b = 20;
 
-    let steps = a + b; // total number of step it takes to reach the right bottom in any way (since we have 21x21 points)
+    let steps = a+a; // total number of step it takes to reach the right bottom in any way (since we have 21x21 points)
 
     // now we need to answer the question
     // in the case of 3x3 grid
-    // we have 6 places and 3 ways of each direction. How many combinations are there considering that we don't want any duplications (order doesn't matters)
+    // we have 6 places and 3 ways for each direction. How many combinations are there considering that we don't want any duplications (order doesn't matters)
     // so we use basic combinatorics formula C(n, k)= n!/(n-k)! * k! , where n = total moves required, and k=limit for each direction
 
     // ultimate formula
@@ -24,6 +23,7 @@ fn main(){
 
     // formula after algebraic stuff exactly for this case
     let answer = fac(steps, 21)/fac(a, 2);
+    
     println!("Answer: {} steps", answer);
     println!("Should be: 137846528820");
 
