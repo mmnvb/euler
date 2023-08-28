94a99af54079264ce74852a1817dd4abf9ad3671
Saidgani Usmonov <121709409+SaidganiUsm@users.noreply.github.com>
2023-08-28 18:56:46 +0500

    Create Said.cs

---
+++ 1/050/Said.cs
@@ -0,0 +1,21 @@
+class Program
+{
+  // Prime number check only for now
+    static bool IsPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
