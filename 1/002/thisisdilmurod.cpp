c94dae0c0561e88a9003aeb5c91518d258963cca
thisisdilmurod <dilmurod.abdusamadov2004@gmail.com>
2024-01-18 10:25:57 +0500

    Solved 4

--- 1/002/thisisdilmurod.cpp
+++ 1/002/thisisdilmurod.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
     ll ans = 0;
     int n1 = 1, n2 = 2;
+
     while (n2 < 4000000) {
         if (n2 % 2 == 0) {
             ans += n2;
@@ -15,5 +16,6 @@ int main() {
         n2 += n1;
         n1 = temp;
     }
+    
     cout << ans << endl;
 }
